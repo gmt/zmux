6558019dc128d6635144c72e9a0a4126710c09c3
Greg Turner <gmt@be-evil.net>
2026-03-24 16:52:00 -0700

    Add colour parsing and palette helpers

--- src/zmux.zig
+++ src/zmux.zig
@@ -264,6 +264,7 @@ pub fn getversion() []const u8 {
 
 test {
     _ = @import("attributes.zig");
+    _ = @import("colour.zig");
     _ = @import("hyperlinks.zig");
     _ = @import("names.zig");
     _ = @import("paste.zig");

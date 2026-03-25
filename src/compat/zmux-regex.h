817aa49777608434dd44aa3832dddc745bf91b91
Greg Turner <greg@gmt.rocks>
2026-03-25 08:35:50 -0700

    Add formatter arithmetic and regex helpers

---
+++ src/compat/zmux-regex.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2026 Greg Turner <gmt@pobox.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ZMUX_REGEX_H
+#define ZMUX_REGEX_H
+
+#include <regex.h>
+#include <stddef.h>
+
+void *zmux_regex_new(void);
+int zmux_regex_compile(void *, const char *, int);
+int zmux_regex_exec(void *, const char *, size_t, regmatch_t *);
+void zmux_regex_free(void *);
+
+#endif

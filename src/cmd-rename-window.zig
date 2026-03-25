9891f2952340b3edc43573dbfd6677054a422d4b
Greg Turner <gmt@be-evil.net>
2026-03-24 18:21:13 -0700

    Add session and window rename commands

---
+++ src/cmd-rename-window.zig
@@ -0,0 +1,110 @@
+// Copyright (c) 2026 Greg Turner <gmt@be-evil.net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+// IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+// OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+//
+// Ported in part from tmux/cmd-rename-window.c.
+// Original copyright:
+//   Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
+//   ISC licence – same terms as above.
+
+const std = @import("std");
+const T = @import("types.zig");
+const xm = @import("xmalloc.zig");
+const cmd_mod = @import("cmd.zig");
+const cmdq = @import("cmd-queue.zig");
+const cmd_find = @import("cmd-find.zig");
+const format_mod = @import("format.zig");
+const opts = @import("options.zig");
+const win = @import("window.zig");
+const server_fn = @import("server-fn.zig");
+const notify = @import("notify.zig");
+
+fn exec(cmd: *cmd_mod.Cmd, item: *cmdq.CmdqItem) T.CmdRetval {
+    const args = cmd_mod.cmd_get_args(cmd);
+    var target: T.CmdFindState = .{};
+    if (cmd_find.cmd_find_target(&target, item, args.get('t'), .window, 0) != 0)
+        return .@"error";
+
+    const wl = target.wl orelse return .@"error";
+    const raw_name = args.value_at(0) orelse {
+        cmdq.cmdq_error(item, "new name required", .{});
+        return .@"error";
+    };
+    const new_name = format_mod.format_single(@ptrCast(item), raw_name, cmdq.cmdq_get_client(item), target.s, target.wl, target.wp);
+    defer xm.allocator.free(new_name);
+
+    if (std.mem.indexOf(u8, new_name, "#{") != null) {
+        cmdq.cmdq_error(item, "format expansion not supported yet", .{});
+        return .@"error";
+    }
+
+    win.window_set_name(wl.window, new_name);
+    opts.options_set_number(wl.window.options, "automatic-rename", 0);
+    server_fn.server_status_window(wl.window);
+    notify.notify_window("window-renamed", wl.window);
+    return .normal;
+}
+
+pub const entry: cmd_mod.CmdEntry = .{
+    .name = "rename-window",
+    .alias = "renamew",
+    .usage = "[-t target-window] new-name",
+    .template = "t:",
+    .lower = 1,
+    .upper = 1,
+    .flags = T.CMD_STARTSERVER | T.CMD_AFTERHOOK,
+    .exec = exec,
+};
+
+test "rename-window renames target window and disables automatic rename" {
+    const sess = @import("session.zig");
+    const env_mod = @import("environ.zig");
+
+    sess.session_init_globals(xm.allocator);
+    win.window_init_globals(xm.allocator);
+
+    opts.global_options = opts.options_create(null);
+    defer opts.options_free(opts.global_options);
+    opts.global_s_options = opts.options_create(null);
+    defer opts.options_free(opts.global_s_options);
+    opts.global_w_options = opts.options_create(null);
+    defer opts.options_free(opts.global_w_options);
+    opts.options_default_all(opts.global_options, T.OPTIONS_TABLE_SERVER);
+    opts.options_default_all(opts.global_s_options, T.OPTIONS_TABLE_SESSION);
+    opts.options_default_all(opts.global_w_options, T.OPTIONS_TABLE_WINDOW);
+
+    env_mod.global_environ = env_mod.environ_create();
+    defer env_mod.environ_free(env_mod.global_environ);
+
+    const session_opts = opts.options_create(opts.global_s_options);
+    const session_env = env_mod.environ_create();
+    const s = sess.session_create(null, "rename-window-test", "/", session_env, session_opts, null);
+    defer sess.session_destroy(s, false, "test");
+
+    const w = win.window_create(80, 24, T.DEFAULT_XPIXEL, T.DEFAULT_YPIXEL);
+    var attach_cause: ?[]u8 = null;
+    const wl = sess.session_attach(s, w, -1, &attach_cause).?;
+    const wp = win.window_add_pane(w, null, 80, 24);
+    w.active = wp;
+    s.curw = wl;
+
+    var cause: ?[]u8 = null;
+    const cmd = try cmd_mod.cmd_parse_one(&.{ "rename-window", "-t", "rename-window-test:0", "editor" }, null, &cause);
+    defer cmd_mod.cmd_free(cmd);
+
+    var list: cmd_mod.CmdList = .{};
+    var item = cmdq.CmdqItem{ .client = null, .cmdlist = &list };
+    try std.testing.expectEqual(T.CmdRetval.normal, cmd_mod.cmd_execute(cmd, &item));
+    try std.testing.expectEqualStrings("editor", w.name);
+    try std.testing.expectEqual(@as(i64, 0), opts.options_get_number(w.options, "automatic-rename"));
+}

0bcc10e76a21078fa76ab7b713d9ea027cd6d32a
Greg Turner <greg@gmt.rocks>
2026-03-28 01:40:43 -0700

    Add source-file glob expansion

--- src/c.zig
+++ src/c.zig
@@ -47,6 +47,7 @@ pub const posix_sys = @cImport({
     @cInclude("sys/ioctl.h");
     @cInclude("sys/utsname.h");
     @cInclude("fcntl.h");
+    @cInclude("glob.h");
     @cInclude("regex.h");
     @cInclude("string.h");
     @cInclude("termios.h");
